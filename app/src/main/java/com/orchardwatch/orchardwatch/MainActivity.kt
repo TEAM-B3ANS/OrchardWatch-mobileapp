f828b3e4c2649f9421cec9ff5ae4752ab25dd2fc
Spencer Kocot <spencerkocot@gmail.com>
2019-10-31 13:56:05 -0400

    Upload photo button on bottomAppBar

--- app/src/main/java/com/orchardwatch/orchardwatch/MainActivity.kt
+++ app/src/main/java/com/orchardwatch/orchardwatch/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        webview.loadUrl("https://twitter.com")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
